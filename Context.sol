ff91255cd0bcfce15ab29d81a8abba98469fa620
Muffin Monster <84281003+muffin-monster@users.noreply.github.com>
2021-05-24 14:40:06 +0200

    Muffin Coin Smart Contract

---
+++ Context.sol
@@ -0,0 +1,16 @@
+pragma solidity 0.5.16;
+
+contract Context {
+
+  constructor () internal { }
+
+  function _msgSender() internal view returns (address payable) {
+    return msg.sender;
+  }
+
+  function _msgData() internal view returns (bytes memory) {
+    this;
+    return msg.data;
+  }
+
+}
